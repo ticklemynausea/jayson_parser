0dcf738d0c36278852aa0456d03467a218613c0e
Mário C <igotbugs@ticklemynausea.net>
2017-04-24 14:41:20 +0100

    added tokenizer with unit tests

---
+++ lib/tokenizer.py
@@ -0,0 +1,123 @@
+from enum import Enum
+import re
+
+class TokenType(Enum):
+    BRACE_LEFT = 0      # {
+    BRACE_RIGHT = 1     # }
+    BRACKET_LEFT = 2    # [
+    BRACKET_RIGHT = 3   # ]
+    COLON = 4           # :
+    COMMA = 5           # ,
+    LITERAL = 6         # true false null
+    NUMBER = 8          # 666
+    STRING = 9          # "benfica"
+
+class Token:
+
+    def __init__(self, tokenType, content = None):
+        self.tokenType = tokenType
+        self.content = content
+
+    def __repr__(self):
+        return "<{0} {1}>".format(self.tokenType, self.content)
+
+    def __eq__(self, other):
+        return self.tokenType == other.tokenType and self.content == other.content
+
+class Tokenizer(object):
+
+    def __init__(self, string):
+        self.string = string.strip()
+        self.cursor = 0
+        self.tokens = []
+
+    # returns and consumes the next token in the string
+    def next(self):
+
+        # are we done?
+        if not self.cursor < len(self.string):
+            return None
+
+        # skip whitespace characters
+        while self.string[self.cursor].isspace():
+            self.cursor = self.cursor + 1
+
+        # tokenize structural characters
+        looking_at = self.string[self.cursor]
+        if looking_at == '{':
+            token = Token(TokenType.BRACE_LEFT)
+            self.cursor = self.cursor + 1
+            return token
+
+        elif looking_at == '}':
+            token = Token(TokenType.BRACE_RIGHT)
+            self.cursor = self.cursor + 1
+            return token
+
+        elif looking_at == '[':
+            token = Token(TokenType.BRACKET_LEFT)
+            self.cursor = self.cursor + 1
+            return token
+
+        elif looking_at == ']':
+            token = Token(TokenType.BRACKET_RIGHT)
+            self.cursor = self.cursor + 1
+            return token
+
+        elif looking_at == ':':
+            token = Token(TokenType.COLON)
+            self.cursor = self.cursor + 1
+            return token
+
+        elif looking_at == ',':
+            token = Token(TokenType.COMMA)
+            self.cursor = self.cursor + 1
+            return token
+
+        # tokenize literal symbols
+        elif self.string[self.cursor : self.cursor + 4] == 'null':
+            token = Token(TokenType.LITERAL, 'null')
+            self.cursor = self.cursor + 4
+            return token
+
+        elif self.string[self.cursor : self.cursor + 4] == 'true':
+            token = Token(TokenType.LITERAL, 'true')
+            self.cursor = self.cursor + 4
+            return token
+
+        elif self.string[self.cursor : self.cursor + 5] == 'false':
+            token = Token(TokenType.LITERAL, 'false')
+            self.cursor = self.cursor + 5
+            return token
+
+        # numbers and strings are the hardest
+        # try to find a number
+        match = re.match(r'-?[0-9]+', self.string[self.cursor:])
+        if match:
+            matched_text = match.group(0)
+            token = Token(TokenType.NUMBER, matched_text)
+            self.cursor = self.cursor + len(matched_text)
+            return token
+
+        # try strings
+        match = re.match(r'"[^"]*"', self.string[self.cursor:])
+        if match:
+            matched_text = match.group(0)
+            token = Token(TokenType.STRING, matched_text)
+            self.cursor = self.cursor + len(matched_text)
+            return token
+
+        # it should never reach this point when tokenizing, so throw exception
+        raise ValueError("Error tokenizing input")
+
+    def tokenize(self):
+        #print "Tokenizing..."
+        while True:
+            token = self.next()
+            if token is None:
+                break
+
+            #print "next() returns", token
+            self.tokens.append(token)
+
+        return self.tokens
---
+++ tests/tokenizer.py
@@ -0,0 +1,142 @@
+# run me with python -m unittest tests.tokenizer.TestTokenizer
+
+import unittest
+from lib.tokenizer import Tokenizer, Token, TokenType
+
+# @TODO uh i'll deal with that nasty "\"" thing later
+# sample = """
+# {
+#     "yes": true,
+#     "no": false,
+#     "nothing": null,
+#     "number": 123,
+#     "negative_number": -123,
+#     "strings": "A \"string\".\nFor real.",
+#     "object": {"omg": "things"},
+#     "list": [1, "a", {}, []]
+# }
+# """
+
+sample = """
+{
+    "yes": true,
+    "no": false,
+    "nothing": null,
+    "number": 123,
+    "negative_number": -123,
+    "strings": "A' string'.\nFor real.",
+    "object": {"omg": "things"},
+    "list": [1, "a", {}, []]
+}
+"""
+#tokenizer = Tokenizer(sample)
+#x = tokenizer.tokenize()
+
+#print x
+
+
+class TestTokenizer(unittest.TestCase):
+
+    def test_number(self):
+        sample = """
+            666
+        """
+        tokenizer = Tokenizer(sample)
+        tokens = tokenizer.tokenize()
+
+        self.assertEqual(len(tokens), 1)
+        self.assertEqual(tokens[0], Token(TokenType.NUMBER, '666'))
+
+    def test_string(self):
+        sample = """
+            "xyzzy" "foobar"
+        """
+        tokenizer = Tokenizer(sample)
+        tokens = tokenizer.tokenize()
+
+        self.assertEqual(len(tokens), 2)
+        self.assertEqual(tokens[0], Token(TokenType.STRING, '"xyzzy"'))
+        self.assertEqual(tokens[1], Token(TokenType.STRING, '"foobar"'))
+
+    def test_literal(self):
+        sample = """
+            true false null
+        """
+        tokenizer = Tokenizer(sample)
+        tokens = tokenizer.tokenize()
+
+        self.assertEqual(len(tokens), 3)
+        self.assertEqual(tokens[0], Token(TokenType.LITERAL, 'true'))
+        self.assertEqual(tokens[1], Token(TokenType.LITERAL, 'false'))
+        self.assertEqual(tokens[2], Token(TokenType.LITERAL, 'null'))
+
+    def test_mixed(self):
+        sample = """
+            {
+                "yes": true,
+                "no": false,
+                "nothing": null,
+                "number": 123,
+                "negative_number": -123,
+                "strings": "A' string'.\nFor real.",
+                "object": {"omg": "things"},
+                "list": [1, "a", {}, []]
+            }
+        """
+        tokenizer = Tokenizer(sample)
+        tokens = tokenizer.tokenize()
+
+        self.assertEqual(len(tokens), 47)
+        expected_values = [
+            Token(TokenType.BRACE_LEFT),
+            Token(TokenType.STRING, '"yes"'),
+            Token(TokenType.COLON),
+            Token(TokenType.LITERAL, 'true'),
+            Token(TokenType.COMMA),
+            Token(TokenType.STRING, '"no"'),
+            Token(TokenType.COLON),
+            Token(TokenType.LITERAL, 'false'),
+            Token(TokenType.COMMA),
+            Token(TokenType.STRING, '"nothing"'),
+            Token(TokenType.COLON),
+            Token(TokenType.LITERAL, 'null'),
+            Token(TokenType.COMMA),
+            Token(TokenType.STRING, '"number"'),
+            Token(TokenType.COLON),
+            Token(TokenType.NUMBER, '123'),
+            Token(TokenType.COMMA),
+            Token(TokenType.STRING, '"negative_number"'),
+            Token(TokenType.COLON),
+            Token(TokenType.NUMBER, '-123'),
+            Token(TokenType.COMMA),
+            Token(TokenType.STRING, '"strings"'),
+            Token(TokenType.COLON),
+            Token(TokenType.STRING, '"A\' string\'.\nFor real."'),
+            Token(TokenType.COMMA),
+            Token(TokenType.STRING, '"object"'),
+            Token(TokenType.COLON),
+            Token(TokenType.BRACE_LEFT),
+            Token(TokenType.STRING, '"omg"'),
+            Token(TokenType.COLON),
+            Token(TokenType.STRING, '"things"'),
+            Token(TokenType.BRACE_RIGHT),
+            Token(TokenType.COMMA),
+            Token(TokenType.STRING, '"list"'),
+            Token(TokenType.COLON),
+            Token(TokenType.BRACKET_LEFT),
+            Token(TokenType.NUMBER, '1'),
+            Token(TokenType.COMMA),
+            Token(TokenType.STRING, '"a"'),
+            Token(TokenType.COMMA),
+            Token(TokenType.BRACE_LEFT),
+            Token(TokenType.BRACE_RIGHT),
+            Token(TokenType.COMMA),
+            Token(TokenType.BRACKET_LEFT),
+            Token(TokenType.BRACKET_RIGHT),
+            Token(TokenType.BRACKET_RIGHT),
+            Token(TokenType.BRACE_RIGHT)
+        ]
+        self.assertEqual(len(expected_values), 47)
+        self.assertEqual(len(tokens), 47)
+        for i in range(0, len(expected_values)):
+            self.assertEqual(tokens[i], expected_values[i])
